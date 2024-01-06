df7d5abb7bf3e2d4d34f3542e38efd6030f22fa4
Harini <bliinnkkk@github.com>
2024-01-06 11:26:54 +0530

    CreatedPages

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,22 @@
+// eslint-disable-next-line no-unused-vars
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import About from "./pages/About";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import SignOut from "./pages/SignOut";
 
 export default function App() {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signout" element={<SignOut />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
