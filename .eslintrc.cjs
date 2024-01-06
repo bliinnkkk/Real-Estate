c756fec9852b0fa7357bc9dcab068b5e682fd41b
Harini <bliinnkkk@github.com>
2024-01-06 11:26:22 +0530

    ignoring non-used-vars

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,8 +2,8 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
     'plugin:react/recommended',
+    'eslint:recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
   ],
@@ -12,9 +12,12 @@ module.exports = {
   settings: { react: { version: '18.2' } },
   plugins: ['react-refresh'],
   rules: {
+    
     'react-refresh/only-export-components': [
       'warn',
+      
       { allowConstantExport: true },
+      
     ],
   },
 }
